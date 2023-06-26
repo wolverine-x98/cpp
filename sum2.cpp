1e579039b6e434e1ce746b5f4155f08bdaae0a07
wolverine-x98 <137806402+wolverine-x98@users.noreply.github.com>
2023-06-26 21:29:29 +0530

    cpp 3 digit number addition...

---
+++ sum2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,sum=0;
+	cout<<"\n"<<"enter 3 digit number to find sum ";
+	cin>>n;
+	while(n>0)
+	{
+		sum=sum+n%10;
+		n=n/10;
+	}
+	cout<<"\n"<<"sum of digit="<<sum;
+	return 0;
+}
